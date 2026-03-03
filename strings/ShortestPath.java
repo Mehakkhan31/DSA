643ff7ef1be8086cfd91c04bee59cbeb5712967e
Mehakkhan31 <mehakkhan997740@gmail.com>
2026-03-03 16:09:57 +0530

    commit

---
+++ strings/ShortestPath.java
@@ -0,0 +1,16 @@
+// package strings;
+
+// public class ShortestPath {
+//     public static int getShortestPath(String path){
+//         int x = 0, y = 0;
+
+//         for(int i=0; i<path.length(); i++) {
+//             if()
+//         }
+//     }
+    
+
+//     public static void main(String[] args) {
+//         String path = "WEENSENN";
+//     }
+// }

2ff160989d6589939657f3f43825561be1a78d20
Mehakkhan31 <mehakkhan997740@gmail.com>
2026-03-03 16:17:25 +0530

    pattern

---
+++ Pattern/pattern1.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+public class pattern1 {
+    public static void update() {
+        int n = 6;
+
+        for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+   
+        public static void main(String args[]) {
+            update();
+ }   
+}
+
+
---
+++ Pattern/pattern4.java
@@ -0,0 +1,21 @@
+package Patterns;
+
+public class pattern4 {
+    public static void update() {
+        int n = 6;
+
+        for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++){
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+    }
+   
+        public static void main(String args[]) {
+            update();
+ }   
+}
+
+
+
